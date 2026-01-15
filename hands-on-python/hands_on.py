7a29b4b2774be07229a6aaaaf16f7bc578ce4941
Krushna <krushnachandra.dash@walmart.com>
2026-01-15 21:14:55 +0530

    some more code

---
+++ hands-on-python/hands_on.py
@@ -0,0 +1,5 @@
+first_product_price=input("Enter the price of the first product: ")
+secon_product_price=input("Enter the price of the second product: ")
+
+total_price=int(first_product_price)+int(secon_product_price)
+print("The total price of the two products is: ",total_price)
---
+++ linear-models-regression-classification/note.md
@@ -0,0 +1,11 @@
+Supervised Learning
+    -> Classification (linear function )
+    -> Regression 
+
+
+    Classification
+        -> Object identification
+    Regression
+        -> House price predection
+        -> Probalibilty of developing cracks
+
